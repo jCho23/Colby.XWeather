2891c1e6a8bd55906d47335b927e9de2b50e631b
June Cho <junecho.ucb@gmail.com>
2017-12-09 19:43:09 -0500

    Changed APK Path

--- Colby.XWeather/AppInitializer.cs
+++ Colby.XWeather/AppInitializer.cs
@@ -27,7 +27,7 @@ namespace Colby.XWeather
 					.Android
 					// TODO: Update this path to point to your Android app and uncomment the
 					// code if the app is not included in the solution.
-					.ApkFile ("/Users/junecho/Desktop/apkS/Colby.XWeather.apk")
+                    .ApkFile ("/Users/junecho/Desktop/APKs/XWeatherDemo.apk")
 					.StartApp();
 			}
 
